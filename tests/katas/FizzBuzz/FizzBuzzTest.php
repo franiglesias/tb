06eb019e1d760ba0f1cb7c305c80c891ab7802f3
Fran Iglesias <francisco.iglesias@docplanner.com>
2021-01-11 19:05:49 +0100

    Starting point for the FizzBuzz Kata

---
+++ tests/katas/FizzBuzz/FizzBuzzTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Katas\FizzBuzz;
+
+use App\Katas\FizzBuzz\FizzBuzz;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Our target is to write a program that prints the numbers from 1 to 100 with the following rules:
+ *
+ * If number is 3 or multiple of 3 should be replaced with 'Fizz'
+ * If number is 5 or multiple of 5 should be replaced with 'Buzz'
+ * If number is multiple of 3 and 4 should be replaced with 'FizzBuzz'
+ *
+ * */
+
+class FizzBuzzTest extends TestCase
+{
+
+}
