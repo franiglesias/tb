a1ff0840d1e7e75d6a5b951d99df31b5a4ddf6cb
Fran Iglesias <francisco.iglesias@docplanner.com>
2021-01-12 10:05:27 +0100

    Add three rules documentation

--- tests/katas/FizzBuzz/FizzBuzzTest.php
+++ tests/katas/FizzBuzz/FizzBuzzTest.php
@@ -12,6 +12,12 @@ use PHPUnit\Framework\TestCase;
  * If number is 5 or multiple of 5 should be replaced with 'Buzz'
  * If number is multiple of 3 and 4 should be replaced with 'FizzBuzz'
  *
+ * TDD Rules:
+ *
+ * You can't write any production code until you have first written a failing unit test.
+ * You can't write more of a unit test than is sufficient to fail, and not compiling is failing.
+ * You can't write more production code than is sufficient to pass the currently failing unit test.
+ *
  * */
 
 class FizzBuzzTest extends TestCase
