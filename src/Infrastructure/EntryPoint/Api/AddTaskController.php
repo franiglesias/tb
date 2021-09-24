e9709ea10a55652172b15cf559dc4194cde9d017
Fran Iglesias <franiglesias@mac.com>
2021-09-24 11:10:04 +0200

    Acceptance E2E test and minimum controller

---
+++ src/Infrastructure/EntryPoint/Api/AddTaskController.php
@@ -0,0 +1,13 @@
+<?php
+declare (strict_types=1);
+
+namespace App\Infrastructure\EntryPoint\Api;
+
+class AddTaskController
+{
+    public function __invoke()
+    {
+        throw new \RuntimeException('Implement __invoke() method.');
+    }
+
+}
--- tests/Katas/TodoList/TodoListAcceptanceTest.php
+++ tests/Katas/TodoList/TodoListAcceptanceTest.php
@@ -8,8 +8,40 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TodoListAcceptanceTest extends WebTestCase
 {
+    /** @test */
+    public function asUserICanAddATaskToTheList(): void
+    {
+        $client = self::createClient();
 
+        $payload = [
+            'task' => 'Write a test that fails'
+        ];
+        $client->request(
+            'POST',
+            '/api/todo',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($payload, JSON_THROW_ON_ERROR)
+        );
 
+        $client->request(
+            'GET',
+            '/api/todo',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json']
+        );
+        $response = $client->getResponse();
+        $contents = $response->getContent();
+        $tasks = json_decode($contents, true);
+
+        $expected = [
+            '[ ] 1. Write a test that fails',
+        ];
+
+        self::assertEquals($expected, $tasks);
+    }
 
     protected function setUp(): void
     {
@@ -27,6 +59,5 @@ class TodoListAcceptanceTest extends WebTestCase
             unlink('repository.data');
         }
     }
-
 
 }
