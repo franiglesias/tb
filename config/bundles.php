7302a37586f0d0b6fd9a9f432a50f37e85510431
Fran Iglesias <franiglesias@mac.com>
2021-10-11 18:21:14 +0200

    Introduce and configure behat and phpspec. Create design folder to group features and specs.

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
 ];
