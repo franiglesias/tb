5f537eefe64ac3f666a052b5eba96d07cbd3acb5
Fran Iglesias <franiglesias@mac.com>
2022-02-25 17:05:06 +0100

    Code style fixings

--- src/Lib/FileStorageEngine.php
+++ src/Lib/FileStorageEngine.php
@@ -4,34 +4,57 @@ declare(strict_types=1);
 
 namespace App\Lib;
 
-
 class FileStorageEngine
 {
     private string $filePath;
 
-    public function __construct($filePath)
+    public function __construct(string $filePath)
     {
         $this->filePath = $filePath;
     }
 
+    /** @return array<object> */
     public function loadObjects(): array
     {
         if (!file_exists($this->filePath)) {
             return [];
         }
 
-        $file = fopen($this->filePath, 'rb');
-        $objects = unserialize(fgets($file), ['allowed_classes' => true]);
+        $file = $this->openFileForReading();
+
+        $objects = unserialize((string)fgets($file), ['allowed_classes' => true]);
         fclose($file);
 
         return $objects;
     }
 
+    /** @param array<object> $objects */
     public function persistObjects(array $objects): void
     {
-        $file = fopen($this->filePath, 'wb');
+        $file = $this->openFileForWriting();
         fwrite($file, serialize($objects));
         fclose($file);
     }
 
+    /** @return resource */
+    protected function openFileForReading()
+    {
+        $file = fopen($this->filePath, 'rb');
+        if (!$file) {
+            throw new \RuntimeException(sprintf('Unable to open file %s for reading', $this->filePath));
+        }
+
+        return $file;
+    }
+
+    /** @return resource */
+    protected function openFileForWriting()
+    {
+        $file = fopen($this->filePath, 'wb');
+        if (!$file) {
+            throw new \RuntimeException(sprintf('Unable to open file %s for writing', $this->filePath));
+        }
+
+        return $file;
+    }
 }
